ffc899bd7f4f8412ce069f450b7fdea2cb91e7f4
Charles Joseph Ballesteros <cjosephballesteros@gmail.com>
2026-02-14 22:23:15 -0800

    Added colorblind mode and fixed modals. Updated raidbosses.json and some characters.json

--- frontend/app/game/guess-table/guess-table.ts
+++ frontend/app/game/guess-table/guess-table.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, inject} from '@angular/core';
 import { GuessResponse } from '../searchjson/searchjson.model';
 import { NgClass } from '@angular/common';
+import { optionsService } from '../../services/options.service';
 
 @Component({
   selector: 'app-guess-table',
@@ -10,4 +11,33 @@ import { NgClass } from '@angular/common';
 })
 export class GuessTableComponent {
   @Input() guesses: GuessResponse[] = [];
+
+  colorblindMode = inject(optionsService);
+
+  // this is really ugly we should change this eventually
+  cellBgBinary(g: any){
+    const isMatch = g;
+    const isColorblind = this.colorblindMode.getMode();
+
+    if (!isMatch) return 'bg-red-200';
+
+    return isColorblind ? 'bg-blue-200' : 'bg-green-200';
+  }
+  cellBgThree(g: any){
+    const isMatch = g;
+    const isColorblind = this.colorblindMode.getMode();
+
+    if (isMatch === 'partial') return 'bg-yellow-200';
+    else if (isMatch === 'none') return 'bg-red-200';
+
+    return isColorblind ? 'bg-blue-200' : 'bg-green-200';
+  }
+  cellBgNumeric(g: any){
+    const isMatch = g;
+    const isColorblind = this.colorblindMode.getMode();
+
+    if (isMatch !== 'equal') return 'bg-red-200';
+
+    return isColorblind ? 'bg-blue-200' : 'bg-green-200';
+  }
 }
--- frontend/app/game/header-buttons/header-buttons.ts
+++ frontend/app/game/header-buttons/header-buttons.ts
@@ -1,13 +1,15 @@
 import {Component, ViewChild, Input} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import { HTPComponent } from './htp/htp';
+import {OptionsComponent} from './options/options';
 
 @Component({
   selector: 'app-header-buttons',
-  imports: [ HTPComponent ],
+  imports: [HTPComponent, OptionsComponent],
   templateUrl: './header-buttons.html',
   styleUrl: './header-buttons.css'
 })
 export class HeaderButtons {
-  @Input() open = false;
+  @Input() openOptions = false;
+  @Input() openHelp = false;
 }
