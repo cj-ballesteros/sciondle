b3db7c96f5d03d3c8725283cf3c780215b7fc74e
Charles Joseph Ballesteros <cjosephballesteros@gmail.com>
2026-03-17 21:23:21 -0700

    local storage update for daylight savings time

--- frontend/app/services/guess_storage.service.ts
+++ frontend/app/services/guess_storage.service.ts
@@ -37,7 +37,7 @@ export class GuessStorageService {
       })
     );
 
-    laTime.setHours(laTime.getHours() - 5);
+    laTime.setHours(laTime.getHours() - 4);
     return laTime.toISOString().slice(0, 10);
   }
 
